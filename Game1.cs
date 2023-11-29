2dcb9b88312a3ed57bcb291312278007ec459462
Andrew Monteith <339531824@GoTVDSB.ca>
2023-11-29 11:00:07 -0500

    not working

--- Game1.cs
+++ Game1.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace Animation_with_Classes
 {
@@ -11,14 +13,10 @@ namespace Animation_with_Classes
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D tribbleBrownTexture;
-
-        Texture2D tribbleCreamTexture;
-
+        Song bling;
+        int amount;
         Texture2D tribbleGreyTexture;
         List <Tribble> tribbles = new List<Tribble> ();
-        Texture2D tribbleOrangeTexture;
-
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -26,19 +24,21 @@ namespace Animation_with_Classes
             this.Window.Title = "Lesson 3 - Animation Part 1";
             IsMouseVisible = true;
 
-            _graphics.PreferredBackBufferWidth = 800;  // set this value to the desired width of your window
-            _graphics.PreferredBackBufferHeight = 500;   // set this value to the desired height of your window
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
             _graphics.ApplyChanges();
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            Random generator = new Random();
             base.Initialize();
-            for (int i = 0; i < 20; i++)
+            amount = 3;
+            Random generator = new Random();
+            this.bling = Content.Load<Song>("Bling");
+            for (int i = 0; i < amount; i++)
             {
-                tribbles.Add(new Tribble(tribbleGreyTexture, new Rectangle(generator.Next(20, 400), generator.Next(20, 400), generator.Next(50, 150), generator.Next(50, 150)), new Vector2(generator.Next(1, 4), generator.Next(1, 4))));
+                tribbles.Add(new Tribble(tribbleGreyTexture, bling));
             }
         }
 
@@ -48,13 +48,7 @@ namespace Animation_with_Classes
 
             // TODO: use this.Content to load your game content here
             tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
-            tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
-            tribbleCreamTexture = Content.Load<Texture2D>("tribbleCream");
-            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
-
-
         }
-
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -62,7 +56,7 @@ namespace Animation_with_Classes
 
             // TODO: Add your update logic here
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < amount; i++)
             {
                 tribbles[i].Move(_graphics);
             }
@@ -75,9 +69,9 @@ namespace Animation_with_Classes
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            for (int i = 0;i < 20;i++)
+            for (int i = 0;i < amount;i++)
             {
-                _spriteBatch.Draw(tribbles[i].Texture, tribbles[i].Bounds, Color.White);
+                _spriteBatch.Draw(tribbles[i].Texture, tribbles[i].Bounds, tribbles[i].Color);
             }
             _spriteBatch.End();
             base.Draw(gameTime);
--- Tribble.cs
+++ Tribble.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +14,18 @@ namespace Animation_with_Classes
     class Tribble
     {
         private Texture2D _texture;
-        private Microsoft.Xna.Framework.Rectangle _rectangle;
-        private Microsoft.Xna.Framework.Vector2 _speed;
-
-        public Tribble(Texture2D texture, Microsoft.Xna.Framework.Rectangle rect, Microsoft.Xna.Framework.Vector2 speed)
+        private Rectangle _rectangle;
+        private Vector2 _speed;
+        private Song _song;
+        private Color _color;
+        Random generator = new Random();
+        public Tribble(Texture2D texture, Song sound)
         {
             _texture = texture;
-            _rectangle = rect;
-            _speed = speed;
+            _rectangle = new Rectangle(generator.Next(20, 400), generator.Next(20, 400), generator.Next(50, 100), generator.Next(50, 100));
+            _speed = new Vector2(generator.Next(-4, 4), generator.Next(-4, 4));
+            _song = sound;
+            _color = new Color(Convert.ToByte(generator.Next(255)), Convert.ToByte(generator.Next(255)), Convert.ToByte(generator.Next(255)));
         }
 
         public Texture2D Texture
@@ -29,28 +33,60 @@ namespace Animation_with_Classes
             get { return _texture; }
         }
 
-        public Microsoft.Xna.Framework.Rectangle Bounds
+        public Rectangle Bounds
         {
             get { return _rectangle; }
             set { _rectangle = value; }
         }
 
-        public Microsoft.Xna.Framework.Vector2 Speed
+        public Vector2 Speed
         {
             get { return _speed; }
             set { _speed = value; }
         }
+        public Color Color
+        {
+            get { return _color; }
+        }
 
+        public void PlaySound()
+        {
+            MediaPlayer.Play(_song);
+        }
         public void Move(GraphicsDeviceManager graphics)
         {
             _rectangle.Offset(_speed);
-            if (_rectangle.Right > graphics.PreferredBackBufferWidth || _rectangle.Left < 0)
+            if (_rectangle.Right > graphics.PreferredBackBufferWidth || _rectangle.Left < 0 && _speed.Y!=0)
             {
                 _speed.X *= -1;
+                PlaySound();
+                _color = new Color(Convert.ToByte(generator.Next(255)), Convert.ToByte(generator.Next(255)), Convert.ToByte(generator.Next(255)));
             }
-            if (_rectangle.Bottom > graphics.PreferredBackBufferHeight || _rectangle.Top < 0)
+            else if (_rectangle.Bottom > graphics.PreferredBackBufferHeight || _rectangle.Top < 0 && _speed.X != 0)
             {
                 _speed.Y *= -1;
+                PlaySound();
+                _color = new Color(Convert.ToByte(generator.Next(255)), Convert.ToByte(generator.Next(255)), Convert.ToByte(generator.Next(255)));
+            }
+            else if (_rectangle.Left > graphics.PreferredBackBufferWidth && _speed.Y == 0)
+            {
+                _rectangle = new Rectangle(0 - _rectangle.Width, _rectangle.Top, _rectangle.Width, _rectangle.Height);
+                PlaySound();
+            }
+            else if (_rectangle.Right < 0 && _speed.Y == 0)
+            {
+                _rectangle = new Rectangle(graphics.PreferredBackBufferWidth, _rectangle.Top, _rectangle.Width, _rectangle.Height);
+                PlaySound();
+            }
+            else if (_rectangle.Top > graphics.PreferredBackBufferHeight&& _speed.X == 0)
+            {
+                _rectangle = new Rectangle(_rectangle.Left, 0 - _rectangle.Height, _rectangle.Width, _rectangle.Height);
+                PlaySound();
+            }
+            else if (_rectangle.Bottom < 0 && _speed.X == 0)
+            {
+                _rectangle = new Rectangle(_rectangle.Left, graphics.PreferredBackBufferHeight, _rectangle.Width, _rectangle.Height);
+                PlaySound();
             }
         }
     }
